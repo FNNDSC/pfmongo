54b2ee7166aaedd704e2077c90be949c23a68720
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-09-18 10:37:07 -0400

    Remove legacy 'keys' and change some defaults

--- pfmongo/config/settings.py
+++ pfmongo/config/settings.py
@@ -1,15 +1,10 @@
 import  os
 from    pydantic_settings  import BaseSettings
 
-class Keys(BaseSettings):
-    DBauthPath:str      = '/db/init.json'
-    ReadWriteKey:str    = "local"
-
 class Mongo(BaseSettings):
     MD_URI:str          = "localhost:27017"
-    MD_DB:str           = "default"
+    MD_DB:str           = ""
     MD_username:str     = "username"
     MD_password:str     = "password"
 
-keys            = Keys()
 mongosettings   = Mongo()
