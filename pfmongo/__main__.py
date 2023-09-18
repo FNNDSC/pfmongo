f93f6317bbabb626abb125fa61a418684caf5109
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-09-18 10:36:30 -0400

    Move 'man' stuff to man module

--- pfmongo/__main__.py
+++ pfmongo/__main__.py
@@ -9,9 +9,11 @@
 
 __version__ = "0.9.9"
 
-import sys, os
+import sys
 
 from    pfmongo             import pfmongo
+from    pfmongo             import driver
+from    pfmongo.commands    import fs
 from    pfmongo.pfmongo     import parser_setup, parser_interpret, parser_JSONinterpret
 
 try:
@@ -19,104 +21,119 @@ try:
 except:
     from pfmongo            import __pkg, __version__
 
-import  asyncio
-from    asyncio             import AbstractEventLoop
-
-from    argparse            import RawTextHelpFormatter
 from    argparse            import ArgumentParser, Namespace
 import  pudb
 from    pfmisc              import Colors as C
 from    typing              import Any, Literal
-import  json
-import  re
-NC =  C.NO_COLOUR
+from    pfmongo.config      import settings
+from    pathlib             import Path
+import  appdirs
+import  click
 
-str_title:str = r'''
+try:
+    from    commands      import add, delete, search, show, fs, man
 
-           $$$$$$\
-          $$  __$$\
- $$$$$$\  $$ /  \__|$$$$$$\$$$$\   $$$$$$\  $$$$$$$\   $$$$$$\   $$$$$$\
-$$  __$$\ $$$$\     $$  _$$  _$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\
-$$ /  $$ |$$  _|    $$ / $$ / $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$ /  $$ |
-$$ |  $$ |$$ |      $$ | $$ | $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |
-$$$$$$$  |$$ |      $$ | $$ | $$ |\$$$$$$  |$$ |  $$ |\$$$$$$$ |\$$$$$$  |
-$$  ____/ \__|      \__| \__| \__| \______/ \__|  \__| \____$$ | \______/
-$$ |                                                  $$\   $$ |
-$$ |                                                  \$$$$$$  |
-\__|                                                   \______/
-'''
+except:
+    from    .commands     import add, delete, search, show, fs, man
 
+options:Namespace               = Namespace()
 
-str_heading:str = f"""
-                        python (pf) monogodb client and module
-
-"""
-
-def synopsis(ab_shortOnly = False) -> None:
-    scriptName:str          = os.path.basename(sys.argv[0])
-    print(C.CYAN + '''
-    NAME
-        ''', end = '' + NC)
-    print(scriptName)
-    print(C.CYAN + '''
-    SYNPOSIS
-        ''' + NC, end = '')
-    print(scriptName + pfmongo.package_CLIfull)
-    print(C.CYAN + '''
-    DESCRIPTION ''' + NC, end = '')
-    print(pfmongo.package_description)
-
-    if ab_shortOnly: return
-    print(C.CYAN + '''
-    ARGS''' + NC, end="")
-    print(pfmongo.package_argsSynopsisFull)
-
-def earlyExit_check(args:Namespace) -> int:
+def sysargv_revamp(newarg:list[str]) ->list[str]:
     """
-    Check if version/man page are required, and if so service
-    and return
+    Small but crucial method for "revamping" the sys.argv list that
+    is passed to the main click function.
 
-    Args:
-        args (Namespace): The CLI namespace
+    The new list is returned in addition to being set in the sys
+    module.
 
-    Returns:
-        int: 0 -- continue
-             1 -- exit
+    :param newarg: a string list of "new" arguments
     """
-    if args.man:
-        print(C.GREEN + str_title)
-        str_help:str    = ""
-        if args.man:    synopsis(False)
-        else:           synopsis(True)
-        return 1
-    if args.b_version:
-        print("Name:    ", end="")
-        print(C.LIGHT_CYAN + f'{__pkg.name}' + NC)
-        print("Version: ", end="")
-        print(C.LIGHT_GREEN + f'{__version__}\n')
-        return 1
-    return 0
+    executable:str              = sys.argv[0]
+    sys.argv                    = []
+    sys.argv.append(executable)
+    sys.argv.extend(newarg)
+    return sys.argv
 
-def main(argv=None) -> Literal[1, 0]:
+def main(argv:list[str]=[]) -> int:
+    """
+    The main entry point to the program. Can also be called from Python by
+    passing along a "pseudo" argv list of strings (note the first element of
+    the list MUST be the executable name).
 
+    This function demonstrates a design pattern that mixes argparse and click
+    parsing, hopefully leveraging something of the best of both worlds.
+
+    :param argv: a CLI argv list of strings.
+    """
+    global options
     # pudb.set_trace()
-    # Preliminary setup
+    add_help:bool               = False
     parser:ArgumentParser       = parser_setup(
-                                    'A client for interacting with monogo DBs'
+                                    'A client for interacting with mongo DBs',
+                                    add_help
                                 )
-    options:Namespace           = parser_interpret(parser, argv)
-    if earlyExit_check(options): return 1
 
-    # Create the mongodb object
-    mongodb:pfmongo.Pfmongo     = pfmongo.Pfmongo(options)
+    # Should we show (and exit) some man page help?
+    options, extra              = parser_interpret(parser, argv)
 
-    # and run it!
-    loop:AbstractEventLoop      = asyncio.get_event_loop()
-    loop.run_until_complete(mongodb.service())
+    # Is this correct?
+    if options.DBname:
+        settings.mongosettings.MD_DB    = options.DBname
+    if settings.mongosettings.MD_DB and not options.DBname:
+        options.DBname          = settings.mongosettings.MD_DB
+    URI:str                     = settings.mongosettings.MD_URI.\
+                                        replace(':', '-').\
+                                        replace('/', '')
+    options.configPath          = Path(
+            appdirs.user_config_dir(appname = __pkg.name)
+    )
+    options.thisSession         = options.configPath        / \
+                                    Path("_MONGO_" + URI)   / \
+                                    Path(settings.mongosettings.MD_DB)
+    if man.coreOptions_show(options):
+        return man.man(options)
 
-    print(mongodb.responseData.model_dump_json())
+    # the click "app" interprets sys.argv, hence the revamping here
+    newargv:list[str]           = sysargv_revamp(extra)
 
+    return(app())
+
+@click.group(help = f"""
+
+{pfmongo.package_description}
+
+""")
+@click.option('--man',
+              is_flag   = True,
+              help      = 'show more detail about core OPTIONS')
+@click.pass_context
+def app(ctx:click.Context,
+        man:bool) -> int:
+    """
+    The main "app" -- it mostly serves as a point to route to the
+    various subcommands and provide some in-line help.
+
+    :param ctx: a click "context" -- it is expanded here to transmit additional
+                information to subcommands.
+    :param man: a CLI --man parameter used to dispatch to the argparse handler
+    :return: simply returns a 0:success or <N>:failure
+    """
+
+    global options
+    ctx.obj                     = {}
+    ctx.obj['options']          = options
+
+    subcommand:str|None         = click.get_current_context().invoked_subcommand
+    if subcommand != 'fs':
+        envCheckFailure:int     = driver.env_failCheck(options)
+        if envCheckFailure:
+            return envCheckFailure
     return 0
 
-if __name__ == "__main__":
-    sys.exit(main())
+app.add_command(add.add)
+app.add_command(delete.delete)
+app.add_command(search.search)
+app.add_command(fs.fs)
+app.add_command(show.show)
+#app.add_command(man.man)
+
