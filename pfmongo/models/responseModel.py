0fce93f42ac92bc1d6be5b9ff56285abfbd5cb64
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-09-27 09:10:29 -0400

    Add models for various responses

--- pfmongo/models/responseModel.py
+++ pfmongo/models/responseModel.py
@@ -4,27 +4,53 @@ str_description = """
 
 """
 
+from logging import info
 from    pydantic            import BaseModel, Field
 from    typing              import Optional, List, Dict
 from    datetime            import datetime
 from    enum                import Enum
 from    pathlib             import Path
 
-class mongodbSimple(BaseModel):
-    """The simplest mongodb model POST"""
-    mongodb                              : str   = ""
-
-class mongodbDelete(BaseModel):
-    status                              : bool  = False
-
-class mongodbBoolReturn(BaseModel):
-    status                              : bool  = False
-
 class mongodbResponse(BaseModel):
     """The model returned internally"""
-    status                              : bool  = False
-    message                             : str   = ''
-    response                            : dict  = {}
+    status:bool                     = False
+    message:str                     = ''
+    response:dict                   = {}
+
+class databaseConnectStatus(BaseModel):
+    connected:bool                  = False
+    existsAlready:bool              = False
+    error:str                       = ""
+
+class collectionConnectStatus(BaseModel):
+    connected:bool                  = False
+    existsAlready:bool              = False
+    elements:int                    = 0
+    error:str                       = ""
+
+class databaseNamesUsage(BaseModel):
+    otype:str                       = "accessing database names"
+    databaseNames:list              = []
+    info:databaseConnectStatus      = databaseConnectStatus()
+
+class collectionNamesUsage(BaseModel):
+    otype:str                       = "accessing collection names"
+    databaseNames:list              = []
+    info:collectionConnectStatus    = collectionConnectStatus()
+
+class databaseDesc(BaseModel):
+    otype:str                       = "database"
+    host:str                        = ""
+    port:int                        = -1
+    name:str                        = ""
+    info:databaseConnectStatus      = databaseConnectStatus()
+
+class collectionDesc(BaseModel):
+    otype:str                       = "collection"
+    databaseName:str                = ""
+    name:str                        = ""
+    fullName:str                    = ""
+    info:collectionConnectStatus    = collectionConnectStatus()
 
 class time(BaseModel):
     """A simple model that has a time string field"""
