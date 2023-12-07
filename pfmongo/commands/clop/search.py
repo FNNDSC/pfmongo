f5c25957ce9a6dd59586e145714cb194b6ca3043
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-12-07 09:22:54 -0500

    Add search sub command innards

--- pfmongo/commands/clop/search.py
+++ pfmongo/commands/clop/search.py
@@ -1,10 +1,43 @@
-import click
+import  click
+import  pudb
+from    pfmongo         import  driver
+from    argparse        import  Namespace
+from    pfmongo         import  env
+import  json
+from    typing          import  Union
+from    pfmisc          import  Colors as C
 
-@click.command(help="""
-                        search collection
+NC  = C.NO_COLOUR
+GR  = C.GREEN
+CY  = C.CYAN
 
+from pfmongo.models.dataModel import messageType
+
+
+@click.command(help=f"""
+{C.CYAN}search{NC} all documents in a collection for the union of tags in a
+comma separated list.
+
+The "hits" are returned referenced by the passed "field".
 """)
-@click.option('--document',
-              help  = "A JSON formatted file to save to the collection")
-def search(document:str):
-    print(f"Will add {document}")
+@click.option('--for',
+    type        = str,
+    help        = \
+    "A comma separated list. The logical AND of the search is returned",
+    default     = '_id')
+@click.option('--field',
+    type        = str,
+    help        = \
+    "List the search hits referenced by this field",
+    default     = '_id')
+@click.pass_context
+def list(ctx:click.Context, searchFor:str, field:str) -> int:
+    pudb.set_trace()
+    options:Namespace   = ctx.obj['options']
+    options.do          = 'searchDocument'
+    options.argument    = {
+            "field":        field,
+            "searchFor":    searchFor.split(',')
+    }
+    hits:int              = driver.run(options)
+    return hits
