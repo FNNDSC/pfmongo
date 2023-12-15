b36afe8d4838b77dde8c93552ccaba8123ce0971
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-12-15 09:24:25 -0500

    Update with more settings

--- pfmongo/config/settings.py
+++ pfmongo/config/settings.py
@@ -4,31 +4,36 @@ from    pfmongo.models      import dataModel
 from    typing              import Optional
 
 class Mongo(BaseSettings):
-    MD_URI:str          = "localhost:27017"
-    MD_DB:str           = ""
-    MD_COLLECTION:str   = ""
-    MD_username:str     = "username"
-    MD_password:str     = "password"
-    MD_sessionUser:str  = ""
-    stateDBname:str     = "db.txt"
-    stateColname:str    = "collection.txt"
-    stateDupname:str    = "duplicates.txt"
-    stateHashes:str     = "hashes.txt"
+    MD_URI:str              = "localhost:27017"
+    MD_DB:str               = ""
+    MD_COLLECTION:str       = ""
+    MD_username:str         = "username"
+    MD_password:str         = "password"
+    MD_sessionUser:str      = ""
+    stateDBname:str         = "db.txt"
+    stateColname:str        = "collection.txt"
+    stateDupname:str        = "duplicates.txt"
+    stateHashes:str         = "hashes.txt"
+    flattenSuffix:str       = "-flat"
+    responseTruncDepth:int  = 4
+    responseTruncSize:int   = 6000
 
 class App(BaseSettings):
     logging:dataModel.loggingType = dataModel.loggingType.CONSOLE
-    noDuplicates:bool   = True
-    useHashes:bool      = True
+    noDuplicates:bool       = True
+    noHashing:bool          = True
+    donotFlatten:bool       = True
+    noResponseTruncSize:bool= True
 
-logging_val:Optional[str] = 'CONSOLE'
+logging_val:Optional[str]   = 'CONSOLE'
 if 'LOGGING' in os.environ:
-    logging_val         = os.environ['LOGGING']
+    logging_val             = os.environ['LOGGING']
 logging_enum:dataModel.loggingType  = dataModel.loggingType.CONSOLE
 if logging_val:
     try:
-        logging_enum    = dataModel.loggingType[logging_val]
+        logging_enum        = dataModel.loggingType[logging_val]
     except KeyError:
-        logging_enum    = dataModel.loggingType.CONSOLE
+        logging_enum        = dataModel.loggingType.CONSOLE
 
-mongosettings   = Mongo()
-appsettings     = App(logging = logging_enum)
+mongosettings:Mongo         = Mongo()
+appsettings:App             = App(logging = logging_enum)
