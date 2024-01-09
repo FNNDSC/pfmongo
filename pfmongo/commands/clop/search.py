55659e7e0cb3427bd42ae99d22394de50635bcff
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-09 12:47:31 -0500

    Add logic to search flattened collection

--- pfmongo/commands/clop/search.py
+++ pfmongo/commands/clop/search.py
@@ -6,6 +6,9 @@ from    pfmongo         import  env
 import  json
 from    typing          import  Union
 from    pfmisc          import  Colors as C
+from    pfmongo.config  import  settings
+
+from    pfmongo.commands.clop   import add
 
 NC  = C.NO_COLOUR
 GR  = C.GREEN
@@ -13,6 +16,17 @@ CY  = C.CYAN
 
 from pfmongo.models.dataModel import messageType
 
+# def collection_connect(collection:str, options:Namespace) -> int:
+#     options.do          = 'connectCollection'
+#     options.argument    = collection
+#     return driver.run(options)
+
+# def currentCollection_getName(options:Namespace) -> str:
+#     currentCol:str      = env.collectionName_get(options)
+#     if currentCol.endswith(settings.mongosettings.flattenSuffix):
+#         currentCol = currentCol.rstrip(settings.mongosettings.flattenSuffix)
+#         collection_connect(currentCol, options)
+#     return currentCol
 
 @click.command(help=f"""
 {C.CYAN}search{NC} all documents in a collection for the union of tags in a
@@ -34,10 +48,12 @@ The "hits" are returned referenced by the passed "field".
 def search(ctx:click.Context, target:str, field:str) -> int:
     pudb.set_trace()
     options:Namespace   = ctx.obj['options']
+    thisCollection:str  = add.currentCollection_getName(options)
     options.do          = 'searchDocument'
     options.argument    = {
             "field":        field,
-            "searchFor":    target.split(',')
+            "searchFor":    target.split(','),
+            "collection":   thisCollection
     }
     hits:int              = driver.run(options)
     return hits
