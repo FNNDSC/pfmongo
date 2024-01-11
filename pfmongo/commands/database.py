c621391f8d50d09091ae4e0cdc4ccf30b1d07042
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-01-11 18:11:55 -0500

    Add deletedb

--- pfmongo/commands/database.py
+++ pfmongo/commands/database.py
@@ -4,16 +4,12 @@ from    typing      import  Optional
 from    pfmisc      import  Colors  as C
 import  click
 import  pudb
+from    pfmongo.commands.dbop   import connect, showAll, deleteDB
+
 
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
-
-try:
-    from    dbop    import  connect, showAll
-except:
-    from    .dbop   import  connect, showAll
-
 
 @click.group(help=f"""
              {GR}database {CY}<cmd>{NC} -- database commands
@@ -26,4 +22,4 @@ def database():
 
 database.add_command(connect.connect)
 database.add_command(showAll.showAll)
-
+database.add_command(deleteDB.deleteDB)
