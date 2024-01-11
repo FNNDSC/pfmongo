e3672cf1e29a6d34bfe77e448fe547b530944f9b
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-01-11 18:17:20 -0500

    Refactor with new click design

--- pfmongo/commands/clop/delete.py
+++ pfmongo/commands/clop/delete.py
@@ -1,17 +1,35 @@
 import  click
 import  pudb
-from    pfmongo         import  driver
-from    argparse        import  Namespace
-from    pfmongo         import  env
-import  json
-from    typing          import  Union
-from    pfmisc          import  Colors as C
+from    pfmongo                 import  driver
+from    argparse                import  Namespace
+from    pfmongo                 import  env
+from    pfmisc                  import  Colors as C
+from    pfmongo.commands.clop   import add
+from    pfmongo.config          import settings
+
 
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
 
-from pfmongo.models.dataModel import messageType
+def delete_do(id:str, options:Namespace) -> int:
+    thisCollection:str      = add.currentCollection_getName(options)
+    delFail:int             = document_delete(id, options)
+    if settings.appsettings.donotFlatten or delFail:
+        return delFail
+    options.collectionName  = add.shadowCollection_getName(options)
+    delFail:int             = document_delete(id, options)
+    options.collectionName  = thisCollection
+    add.collection_connect(thisCollection, options)
+    return delFail
+
+def document_delete(id:str, options:Namespace) -> int:
+    if env.env_failCheck(options):
+        return 100
+    options.do          = 'deleteDocument'
+    options.argument    = id
+    rem:int             = driver.run(options)
+    return rem
 
 @click.command(help=f"""
 {C.CYAN}delete{NC} a document from a collection
@@ -26,15 +44,9 @@ session state.
 @click.option('--id',
     type  = str,
     help  = \
-    "If specified, delete the document with the passed 'id'",
+    "Delete the document with the passed 'id'",
     default = '')
 @click.pass_context
 def delete(ctx:click.Context, id:str="") -> int:
     # pudb.set_trace()
-    options:Namespace   = ctx.obj['options']
-    options.do          = 'deleteDocument'
-    if env.env_failCheck(options):
-        return 100
-    options.argument    = id
-    rem:int             = driver.run(options)
-    return rem
+    return delete_do(id, ctx.obj['options'])
