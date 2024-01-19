84272ed208f5dcfd1fe03ceb6f83d1eef767c3e4
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-18 20:47:35 -0500

    Refactor and update for model returns

--- pfmongo/commands/dbop/showAll.py
+++ pfmongo/commands/dbop/showAll.py
@@ -1,23 +1,31 @@
 import  click
-from    argparse    import  Namespace
-from    pathlib     import  Path
-from    pfmisc      import  Colors as C
-from    pfmongo     import  driver
+from    argparse        import  Namespace
+from    pfmisc          import  Colors as C
+from    pfmongo         import  driver, env
+from    pfmongo.models  import  responseModel
 
 NC = C.NO_COLOUR
 GR = C.LIGHT_GREEN
 CY = C.CYAN
 
-@click.command(help=f"""
-               {GR}showall{NC} -- show all databases
+def options_add(options:Namespace) -> Namespace:
+    options.do          = 'showAllDB'
+    return options
 
-This command shows all the databases available in a given mongodb
+def showAll_asInt(options:Namespace) -> int:
+    return driver.run_intReturn(options)
+
+def showAll_asModel(options:Namespace) -> responseModel.mongodbResponse:
+    return driver.run_modelReturn(options)
+
+@click.command(cls = env.CustomCommand, help=f"""
+-- list databases containing data
+
+This command shows all the populated databases available in a given mongodb
 server. It accepts no arguments.
 
 """)
 @click.pass_context
-def showAll(ctx:click.Context) -> None:
+def showAll(ctx:click.Context) -> int:
     # pudb.set_trace()
-    options:Namespace   = ctx.obj['options']
-    options.do          = 'showAllDB'
-    showall:int         = driver.run(options)
+    return showAll_asInt(options_add(ctx.obj['options']))
