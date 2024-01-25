b9d86503a683e2c1d9191cdb8754cf620a33cff0
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-25 12:06:14 -0500

    Update help

--- pfmongo/commands/collection.py
+++ pfmongo/commands/collection.py
@@ -1,6 +1,3 @@
-from    pathlib     import  Path
-from    argparse    import  Namespace
-from    typing      import  Optional
 from    pfmisc      import  Colors  as C
 from    pfmongo.commands.clop   import showAll, connect, deleteCol
 from    pfmongo     import env
@@ -12,9 +9,11 @@ GR  = C.GREEN
 CY  = C.CYAN
 
 @click.group(cls=env.CustomGroup,  help = f"""
+    commands suitable for collection focus: {GR}show, connect, deletecol{NC}
+
     This command group provides mongo {CY}"collection"{NC} level commands,
     primarily used to {CY}connect {NC}to a collection, {CY}showall{NC} collections,
-    and also {CY}delete{NC} collections.
+    and also {CY}deletecol{NC} collections.
 
     Use the {CY}deletecol{NC} subcommand with care, since it will delete
     without asking for confirmation.
