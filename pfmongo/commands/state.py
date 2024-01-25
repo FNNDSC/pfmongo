e4a68b3f39fe3ea638bf8933dff86b4726fdd825
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-25 11:51:54 -0500

    Improve help

--- pfmongo/commands/state.py
+++ pfmongo/commands/state.py
@@ -1,6 +1,4 @@
-from    pathlib     import  Path
-from    argparse    import  Namespace
-from    typing      import  Optional
+from    pfmongo     import  env
 from    pfmisc      import  Colors  as C
 import  click
 import  pudb
@@ -8,19 +6,19 @@ import  pudb
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
+YL  = C.YELLOW
 
 from    pfmongo.commands.stateop    import showAll
 
-#try:
-#    from    dbop    import  connect, showAll
-#except:
-#    from    .dbop   import  connect, showAll
-
-
-@click.group(help=f"""
-             {GR}state {CY}<cmd>{NC} -- internal state commands
+@click.group(cls = env.CustomGroup, help=f"""
+internal state commands
 
 This command group provides commands operating on internal state.
+For the most part, this is simply a {CY}showall{NC} that shows the internal
+state (notably the current database and collection of focus).
+
+More information can be returned by calling {GR}pfmongo{NC} with a
+{YL}--detailedOutput{NC} flag.
 
 """)
 def state():
