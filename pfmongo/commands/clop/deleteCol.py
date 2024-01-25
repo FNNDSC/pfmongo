c80eea8311026894d62b86155e66704729257948
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-25 12:07:25 -0500

    Refactor for new design

--- pfmongo/commands/clop/deleteCol.py
+++ pfmongo/commands/clop/deleteCol.py
@@ -1,44 +1,68 @@
 import  click
-import  pudb
-from    pfmongo         import  driver
 from    argparse        import  Namespace
-from    pfmongo         import  env
-import  json
-from    typing          import  Union
+from    pfmongo         import  env, driver
 from    pfmisc          import  Colors as C
-from    pfmongo.commands.clop         import  add
+
+from    pfmongo.commands.clop       import connect as collection
+from    pfmongo.models              import responseModel
+import  pudb
 
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
+PL  = C.PURPLE
+YL  = C.YELLOW
 
 from pfmongo.models.dataModel import messageType
 
-def collection_delete(collection:str, options:Namespace) -> int:
+
+def options_add(database:str, options:Namespace) -> Namespace:
     options.do          = 'deleteCollection'
+    options.argument    = database
+    return options
+
+def col_connectToTarget(options:Namespace) -> str:
+    currentCol:str      = env.collectionName_get(options)
+    if currentCol != options.argument:
+        options.do      = 'connectCollection'
+        collection.connectTo_asInt(options)
+    return options.argument
+
+def collectiondel_setup(options: Namespace) -> int:
     if env.env_failCheck(options):
         return 100
-    collection_connectToTarget(collection, options)
-    options.argument    = collection
-    rem:int             = driver.run(options)
-    return rem
+    col_connectToTarget(options)
+    options.do          = 'deleteCollection'
+    return 0
 
-def collection_connectToTarget(collection:str, options:Namespace) -> str:
-    currentCol:str  = env.collectionName_get(options)
-    if currentCol != collection:
-        add.collection_connect(collection, options)
-    return currentCol
+def collectiondel_asInt(options:Namespace) -> int:
+    fail:int            = 0
+    if (fail := collectiondel_setup(options)):
+        return fail
+    return driver.run_intReturn(options)
+
+def collectiondel_asModel(options:Namespace) -> responseModel.mongodbResponse:
+    model:responseModel.mongodbResponse = responseModel.mongodbResponse()
+    fail:int            = 0
+    if (fail := collectiondel_setup(options)):
+        model.message   = 'env failure'
+        return model
+    return driver.run_modelReturn(options)
 
 @click.command(cls=env.CustomCommand, help=f"""
-Deletes an entire <COLLECTION>
+delete an entire {PL}COLLECTION{NC}
 
-Use with care since there is no delete confirmation!
+SYNOPSIS
+{CY}deletecol {YL}<COLLECTION>{NC}
 
+DESC
+This subcommand removes an entire {YL}COLLECTION{NC} immediately.
+Use with care! No confirmation is asked by the system!
 """)
 @click.argument('collection',
                 required = True)
 @click.pass_context
 def deleteCol(ctx:click.Context, collection:str) -> int:
     # pudb.set_trace()
-    options:Namespace   = ctx.obj['options']
-    return collection_delete(collection, options)
+    # options:Namespace   = ctx.obj['options']
+    return collectiondel_asInt(options_add(collection, ctx.obj['options']))
