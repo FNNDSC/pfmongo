ab0a95225beb84a4302e40994b841965fd982aad
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-25 12:00:47 -0500

    Refactor and update help

--- pfmongo/commands/docop/get.py
+++ pfmongo/commands/docop/get.py
@@ -1,30 +1,50 @@
 import  click
 import  pudb
-from    pfmongo         import  driver
+from    pfmongo         import  driver, env
 from    argparse        import  Namespace
-from    pfmongo         import  env
-import  json
-from    typing          import  Union
+from    pfmongo.models  import  responseModel
 from    pfmisc          import  Colors as C
 
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
+PL  = C.PURPLE
+YL  = C.YELLOW
 
 from pfmongo.models.dataModel import messageType
 
-def document_get(id:str, options:Namespace) -> int:
+def options_add(id:str, options:Namespace) -> Namespace:
     options.do          = 'getDocument'
+    options.argument    = id
+    return options
+
+def get_envFailCheck(options:Namespace) -> int:
     if env.env_failCheck(options):
         return 100
-    options.argument    = id
-    read:int            = driver.run(options)
-    return read
+    return 0
 
-@click.command(help=f"""
-{C.CYAN}get{NC} a document from a collection
+def documentGet_asInt(options) -> int:
+    fail:int    = 0
+    if(fail := get_envFailCheck(options)):
+        return fail
+    return driver.run_intReturn(options)
 
-This subcommand gets a document with passed 'id' from a collection.
+def documentGet_asModel(options) -> responseModel.mongodbResponse:
+    model:responseModel.mongodbResponse = responseModel.mongodbResponse()
+    if get_envFailCheck(options):
+        model.message   = 'env failure'
+        return model
+    return driver.run_modelReturn(options)
+
+@click.command(cls = env.CustomCommand, help=f"""
+read and print {PL}document{NC} from a collection
+
+SYNOPSIS
+{CY}get {YL}--id <id>{NC}
+
+DESC
+This subcommand gets a document with passed 'id' from a collection and
+prints it to the console. Use shell redirection to save to disk.
 
 The "location" is defined by the core parameters, 'useDB' and 'useCollection'
 which are typically defined in the CLI, in the system environment, or in the
@@ -34,9 +54,9 @@ session state.
 @click.option('--id',
     type  = str,
     help  = \
-    "If specified, get the document with the passed 'id'",
+    "the document 'id' to get",
     default = '')
 @click.pass_context
 def get(ctx:click.Context, id:str="") -> int:
     # pudb.set_trace()
-    return document_get(id, ctx.obj['options'])
+    return documentGet_asInt(options_add(id, ctx.obj['options']))
