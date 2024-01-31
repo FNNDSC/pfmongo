20f61f601e6b59ed0b19726c7f15c932614aac07
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-31 08:59:06 -0500

    Add 'fs' in own model file

--- pfmongo/models/fsModel.py
+++ pfmongo/models/fsModel.py
@@ -10,17 +10,10 @@ from    datetime            import datetime
 from    enum                import Enum
 from    pathlib             import Path
 
-class messageType(Enum):
-    """ message type/level """
-    INFO    = 1
-    ERROR   = 2
+class cdResponse(BaseModel):
+    path:Path       = Path("")
+    status:bool     = False
+    error:str       = ""
+    code:int        = 0
 
-class loggingType(Enum):
-    """ logging type """
-    CONSOLE = 1
-    NDJSON  = 2
-
-class time(BaseModel):
-    """A simple model that has a time string field"""
-    time                                : str
 
