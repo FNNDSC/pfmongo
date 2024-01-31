54e60c41a9c153e0290aab629af45c9037f79c7b
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-01-31 08:59:42 -0500

    Refactor with more standard design

--- pfmongo/commands/fop/cd.py
+++ pfmongo/commands/fop/cd.py
@@ -3,7 +3,7 @@ import  click
 from    pfmongo         import  driver, env
 from    argparse        import  Namespace
 from    pfmisc          import  Colors as C
-from    pfmongo.models  import  responseModel, dataModel
+from    pfmongo.models  import  responseModel, fsModel
 from    pathlib         import  Path
 import  pudb
 
@@ -13,45 +13,82 @@ from    pfmongo.commands.database   import showAll as mdb
 from    pfmongo.commands.database   import connect as dbconnect
 from    pfmongo.commands.collection import showAll as col
 from    pfmongo.commands.collection import connect as colconnect
-import  pfmongo.commands.smash                   as smash
+import  pfmongo.commands.smash                     as smash
+import  copy
 
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
 YL  = C.YELLOW
 
-def fullPath_resolve(options:Namespace, path:Path) -> Path:
-    if path.is_absolute(): return path
-    path = smash.cwd(options) / path
-    return path
+def options_add(path:str, options:Namespace) -> Namespace:
+    localoptions            = copy.deepcopy(options)
+    # localoptions.beQuiet    = True
+    localoptions.do         = 'cd'
+    localoptions.path       = Path(path)
+    return localoptions
 
-def path_isValid(options:Namespace, path:Path) -> dataModel.fsPath:
+def fullPath_resolve(options:Namespace) -> Namespace:
+    if options.path.is_absolute(): return options
+    options.path = smash.cwd(options) / options.path
+    return options
+
+def db_isListed(db:str, options:Namespace) -> fsModel.cdResponse:
+    fsPath:fsModel.cdResponse   = fsModel.cdResponse()
+    if db not in mdb.showAll_asModel(driver.settmp(options,[
+                                                    {'do': 'showAllDB'}
+                                                    ])).message:
+        fsPath.error    = f"database '{db}' not in mongo"
+        fsPath.code     = 2
+
+
+def path_connect(options:Namespace) -> fsModel.cdResponse:
+    path:Path   = options.path
     # need to track cwd
-    fsPath:dataModel.fsPath     = dataModel.fsPath()
+    fsPath:fsModel.cdResponse = fsModel.cdResponse()
     if len(path.parents) > 2:
         fsPath.error    = "Path too long"
+        fsPath.code     = 1
         return fsPath
-    (db, collection)    = path_parts(options, path)
-    if db not in mdb.showAll_asModel(options).message:
-        fsPath.error    = f"database {db} not in mongo"
+    (db, collection)    = path_to_dbCol(options)
+    fsPath.path         = options.path
+    if db not in mdb.showAll_asModel(driver.settmp(options,[
+                                                    {'do': 'showAllDB'}
+                                                    ])).message:
+        fsPath.error    = f"database '{db}' not in mongo"
+        fsPath.code     = 2
         return fsPath
     if (dbConnectFail:=dbconnect.connectTo_asInt(dbconnect.options_add(db, options))):
-        fsPath.error    = f"could not connect to database {db}"
+        fsPath.error    = f"could not connect to database '{db}'"
+        fsPath.code     = 3
         return fsPath
     if collection not in col.showAll_asModel(options).message:
-        fsPath.error    = f"collection {collection} not in database {db}"
+        fsPath.error    = f"collection '{collection}' not in database '{db}'"
+        fsPath.code     = 4
     if (colCollectFail:=colconnect.connectTo_asInt(colconnect.options_add(collection, options))):
-        fsPath.error    = "could not connect to collection"
+        fsPath.error    = f"could not connect to collection '{collection}'"
+        fsPath.code     = 5
         return fsPath
     fsPath.status   = True
     fsPath.mesasge  = f"successfully changed path to {path}"
+    fsPath.code     = 0
     return fsPath
 
-def path_parts(options:Namespace, path:Path) -> tuple:
-    if not path_isValid(options, path):
-        return ()
-    (root, db, collection) = path.parts
+def path_to_dbCol(options:Namespace) -> tuple:
+    # if not path_isValid(options, path):
+    #     return ()
+    path:Path       = options.path
+    db:str          = ""
+    collection:str  = ""
+    match len(path.parts):
+        case _ if len(path.parts) >=2:
+            (root, db)  = path.parts
+        case _ if len(path.parts) > 2:
+            (root, db, collection) = path.parts
     return (db, collection)
+
+def path_lengthOK(options:Namespace) -> bool:
+    return True if len(options.path.parents.parts == 2) else False
 
 def path_set(options:Namespace, path:Path) -> Namespace:
     dbname:str      = ""
@@ -67,6 +104,15 @@ def path_set(options:Namespace, path:Path) -> Namespace:
             options,
             [{'DBname'           : dbname},
              {'collectionName'   : collection}])
+
+def changeDirectory(options:Namespace) -> int:
+    options             = fullPath_resolve(options)
+    currentpath:Path    = smash.cwd(options)
+    if (cdResponse:=path_connect(options)).code:
+        print(cdResponse.error)
+        # return 1
+    # path_set(options, target)
+    return cdResponse.code
 
 @click.command(cls = env.CustomCommand, help=f"""
 change directory
@@ -84,12 +130,14 @@ mongodb, trees are not especially nested.
 @click.argument('path',
                 required = True)
 @click.pass_context
-def cd(ctx:click.Context, path:str) -> None:
+def cd(ctx:click.Context, path:str) -> int:
     pudb.set_trace()
-    options         = ctx.obj['options']
-    target:Path     = fullPath_resolve(options, Path(path))
-    path_isValid(options, target)
-    cpath:Path      = smash.cwd(options)
-    path_set(options, target)
+    return changeDirectory(options_add(path, ctx.obj['options']))
+    # target:Path         = fullPath_resolve(options)
+    # currentpath:Path    = smash.cwd(options)
+    # if (cdResponse:=path_connect(options, target)).code:
+    #     print(cdResponse.error)
+    #     return 1
+    # path_set(options, target)
 
 
