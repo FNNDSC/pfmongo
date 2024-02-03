7b29ca375dff028ed6c3c515640f121f346bcfbb
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2024-02-03 11:47:06 -0500

    Major refactor, all add --create

--- pfmongo/commands/fop/cd.py
+++ pfmongo/commands/fop/cd.py
@@ -15,129 +15,183 @@ from    pfmongo.commands.collection import showAll as col
 from    pfmongo.commands.collection import connect as colconnect
 import  pfmongo.commands.smash                     as smash
 import  copy
+from    types                       import SimpleNamespace
 
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
 YL  = C.YELLOW
 
-def options_add(path:str, options:Namespace) -> Namespace:
-    localoptions            = copy.deepcopy(options)
+def options_add(path:str, create:bool, options:Namespace) -> Namespace:
+    localoptions:Namespace  = copy.deepcopy(options)
     # localoptions.beQuiet    = True
     localoptions.do         = 'cd'
-    localoptions.path       = Path(path)
+    localoptions.cd         = SimpleNamespace(**{
+                                "path":     Path(path),
+                                "create":   create
+                            })
     return localoptions
 
 def fullPath_resolve(options:Namespace) -> Namespace:
-    if options.path.is_absolute(): return options
-    options.path = smash.cwd(options) / options.path
+    if options.cd.path.is_absolute():   return options
+    if options.cd.path == Path('.'):
+        options.cd.path = smash.cwd(options)
+    elif smash.cwd(options) == Path('/'):
+        options.cd.path = Path('/') / options.cd.path
+    else:
+        options.cd.path = smash.cwd(options) / options.cd.path
     return options
 
 def db_isListed(db:str, options:Namespace) -> fsModel.cdResponse:
+    fsPath:fsModel.cdResponse       = fsModel.cdResponse()
+    if db not in mdb.showAll_asModel(driver.settmp(options,[
+                                                    {'do': 'showAllDB'}
+                                                    ])).message:
+        fsPath.error                = f"database '{db}' not in mongo"
+        fsPath.status               = False
+        fsPath.code                 = 2
+        fsPath.state["database"]    = 'void'
+    else:
+        fsPath.state["database"]    = 'exists'
+    return fsPath
+
+def db_connect(db:str, options:Namespace) -> fsModel.cdResponse:
     fsPath:fsModel.cdResponse   = fsModel.cdResponse()
-    if db not in mdb.showAll_asModel(driver.settmp(options,[
-                                                    {'do': 'showAllDB'}
-                                                    ])).message:
-        fsPath.error    = f"database '{db}' not in mongo"
-        fsPath.code     = 2
-
-
-def path_connect(options:Namespace) -> fsModel.cdResponse:
-    path:Path   = options.path
-    # need to track cwd
-    fsPath:fsModel.cdResponse = fsModel.cdResponse()
-    if len(path.parents) > 2:
-        fsPath.error    = "Path too long"
-        fsPath.code     = 1
-        return fsPath
-    (db, collection)    = path_to_dbCol(options)
-    fsPath.path         = options.path
-    if db not in mdb.showAll_asModel(driver.settmp(options,[
-                                                    {'do': 'showAllDB'}
-                                                    ])).message:
-        fsPath.error    = f"database '{db}' not in mongo"
-        fsPath.code     = 2
-        return fsPath
+    fsPath.path         = options.cd.path
     if (dbConnectFail:=dbconnect.connectTo_asInt(dbconnect.options_add(db, options))):
         fsPath.error    = f"could not connect to database '{db}'"
+        fsPath.status   = False
         fsPath.code     = 3
         return fsPath
-    if collection not in col.showAll_asModel(options).message:
-        fsPath.error    = f"collection '{collection}' not in database '{db}'"
-        fsPath.code     = 4
-    if (colCollectFail:=colconnect.connectTo_asInt(colconnect.options_add(collection, options))):
-        fsPath.error    = f"could not connect to collection '{collection}'"
-        fsPath.code     = 5
-        return fsPath
-    fsPath.status   = True
-    fsPath.mesasge  = f"successfully changed path to {path}"
-    fsPath.code     = 0
     return fsPath
+
+def col_isListed(db:str, collection:str, options:Namespace) -> fsModel.cdResponse:
+    fsPath:fsModel.cdResponse       = fsModel.cdResponse()
+    if collection not in col.showAll_asModel(driver.settmp(options, [
+                                                            {'do': 'showAllCollections'}
+                                                            ])).message:
+        fsPath.error                = f"collection '{collection}' not in database '{db}'"
+        fsPath.status               = False
+        fsPath.code                 = 4
+        fsPath.state["collection"]  = 'void'
+    else:
+        fsPath.state["collection"]  = 'exists'
+    return fsPath
+
+def col_connect(db:str, collection:str, options:Namespace) -> fsModel.cdResponse:
+    fsPath:fsModel.cdResponse   = fsModel.cdResponse()
+    fsPath.path         = options.cd.path
+    if (colCollectFail:=colconnect.connectTo_asInt(colconnect.options_add(collection, options))):
+        fsPath.error    = f"could not connect to collection '{collection}' in databse '{db}'"
+        fsPath.status   = False
+        fsPath.code     = 5
+    return fsPath
+
+def is_root(options:Namespace) -> bool:
+    b_ret:bool  = False
+    if not len(options.cd.path.parents):
+        env.DBname_stateFileSave(options, '')
+        env.collectionName_stateFileSave(options, '')
+        b_ret = True
+    return b_ret
+
+def is_path_too_long(options) -> fsModel.cdResponse:
+    fsPath:fsModel.cdResponse   = fsModel.cdResponse()
+    if len(options.cd.path.parents) > 2:
+        fsPath.error    = "Path too long"
+        fsPath.status   = False
+        fsPath.code     = 1
+    return fsPath
+
+def path_connect(options:Namespace) -> fsModel.cdResponse:
+    fsPath:fsModel.cdResponse   = fsModel.cdResponse()
+    connect:fsModel.cdResponse  = fsModel.cdResponse()
+    connect.path                = options.cd.path
+    if (fsPath:=is_path_too_long(options)).code:
+        return fsPath
+    if is_root(options):
+        return fsPath
+    (db, collection)    = path_to_dbCol(options)
+    if db and not (fsPath:=db_isListed(db, options)).status:
+        if not options.cd.create:
+            return fsPath
+    connect.state["database"]   = fsPath.state["database"]
+    if db and not (fsPath:=db_connect(db, options)).status:
+        return fsPath
+    if collection and not (fsPath:=col_isListed(db, collection, options)).status:
+        if not options.cd.create:
+            return fsPath
+    connect.state["collection"] = fsPath.state["collection"]
+    if collection \
+        and not (fsPath:=col_connect(db, collection, options)).status:
+            return fsPath
+    connect.status   = True
+    connect.message  = f"successfully connected path to {connect.path}"
+    connect.code     = 0
+    return connect
 
 def path_to_dbCol(options:Namespace) -> tuple:
     # if not path_isValid(options, path):
     #     return ()
-    path:Path       = options.path
+    path:Path       = options.cd.path
     db:str          = ""
     collection:str  = ""
     match len(path.parts):
-        case _ if len(path.parts) >=2:
+        case _ if len(path.parts) ==2:
             (root, db)  = path.parts
         case _ if len(path.parts) > 2:
             (root, db, collection) = path.parts
     return (db, collection)
 
 def path_lengthOK(options:Namespace) -> bool:
-    return True if len(options.path.parents.parts == 2) else False
+    return True if len(options.cd.path.parents.parts == 2) else False
 
-def path_set(options:Namespace, path:Path) -> Namespace:
-    dbname:str      = ""
-    collection:str  = ""
-    match len(path.parents):
-        case _ if len(path.parents) >= 1:
-            dbname      = path.parts[1]
-        case 2:
-            collection  = path.parts[2]
-    env.DBname_stateFileSave(options, dbname)
-    env.collectionName_stateFileSave(options, collection)
-    return driver.settmp(
-            options,
-            [{'DBname'           : dbname},
-             {'collectionName'   : collection}])
+# def path_set(options:Namespace, path:Path) -> Namespace:
+#     dbname:str      = ""
+#     collection:str  = ""
+#     match len(path.parents):
+#         case _ if len(path.parents) >= 1:
+#             dbname      = path.parts[1]
+#         case 2:
+#             collection  = path.parts[2]
+#     env.DBname_stateFileSave(options, dbname)
+#     env.collectionName_stateFileSave(options, collection)
+#     return driver.settmp(
+#             options,
+#             [{'DBname'           : dbname},
+#              {'collectionName'   : collection}])
 
-def changeDirectory(options:Namespace) -> int:
-    options             = fullPath_resolve(options)
-    currentpath:Path    = smash.cwd(options)
+def changeDirectory(options:Namespace) -> fsModel.cdResponse:
+    options                         = fullPath_resolve(options)
+    cdResponse:fsModel.cdResponse   = fsModel.cdResponse()
     if (cdResponse:=path_connect(options)).code:
         print(cdResponse.error)
-        # return 1
-    # path_set(options, target)
-    return cdResponse.code
+    return cdResponse
 
 @click.command(cls = env.CustomCommand, help=f"""
 change directory
 
-SYNOPSIS
-{CY}cd {YL}<path>{NC}
+               SYNOPSIS
+{CY}cd {YL}[--create] <path>{NC}
 
 DESC
 The {YL}cd{NC} command "changes directory" within a mongodb between a
-<database> level at the root `/` and a collection within a
-<database>. Since there are only ever two "directories" in a
+{CY}<database>{NC} level at the root `/` and a {CY}<collection>{NC} within that
+{CY}<database>{NC}. Since there are only ever two "directories" in a
 mongodb, trees are not especially nested.
+
+Note that the underlying behaviour is subtly different to a standard
+filesystem, and this 'cd' can also "create" a path/directory with if called
+with {YL}--create{NC}.
 
 """)
 @click.argument('path',
-                required = True)
+                required    = True)
+@click.option('--create',
+                is_flag     = True,
+                help        = 'If set, also create this path if it does not exist.')
 @click.pass_context
-def cd(ctx:click.Context, path:str) -> int:
+def cd(ctx:click.Context, path:str, create:bool) -> int:
     pudb.set_trace()
-    return changeDirectory(options_add(path, ctx.obj['options']))
-    # target:Path         = fullPath_resolve(options)
-    # currentpath:Path    = smash.cwd(options)
-    # if (cdResponse:=path_connect(options, target)).code:
-    #     print(cdResponse.error)
-    #     return 1
-    # path_set(options, target)
-
+    return changeDirectory(options_add(path, create, ctx.obj['options'])).code
 
