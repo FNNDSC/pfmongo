70acd439cfdd1f0fc1ce55c27840dbe6f6b80870
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-02-06 18:29:47 -0500

    Basic inline doc fix... still WIP!

--- pfmongo/commands/fop/exp.py
+++ pfmongo/commands/fop/exp.py
@@ -5,9 +5,10 @@ from    pfmisc  import Colors as C
 NC  = C.NO_COLOUR
 GR  = C.GREEN
 CY  = C.CYAN
+YL  = C.YELLOW
 
 @click.command(help=f"""
-{GR}exp {CY}<pathIN> <pathEX>{NC} -- export <pathIN> to <pathEX>
+export a {YL}document{NC} to a {CY}file{NC}
 
 The {GR}exp{NC} command "exports" a document {CY}pathIN{NC} to the real filesystem
 at {GR}pathEX{NC}.
