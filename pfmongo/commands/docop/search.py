566aab6032197c0e3e1a9010915b9c3484946975
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-03-21 17:29:45 -0400

    Formatting

--- pfmongo/commands/docop/search.py
+++ pfmongo/commands/docop/search.py
@@ -1,35 +1,42 @@
-import  click
-import  pudb
-from    pfmongo                 import  driver, env
-from    argparse                import  Namespace
-from    pfmisc                  import  Colors as C
-from    pfmongo.models          import  responseModel
-from    typing                  import  cast
+import click
+import pudb
+from pfmongo import driver, env
+from argparse import Namespace
+from pfmisc import Colors as C
+from pfmongo.models import responseModel
+from typing import cast
 
-from    pfmongo.commands.clop   import connect
+from pfmongo.commands.clop import connect
 
-NC  = C.NO_COLOUR
-GR  = C.GREEN
-CY  = C.CYAN
-YL  = C.YELLOW
+NC = C.NO_COLOUR
+GR = C.GREEN
+CY = C.CYAN
+YL = C.YELLOW
+PL = C.PURPLE
 
-def options_add(target:str, field:str, options:Namespace) -> Namespace:
-    options.do          = 'searchDocument'
-    options.argument    = {
-            "field":        field,
-            "searchFor":    target.split(','),
-            "collection":   connect.baseCollection_getAndConnect(options).collectionName
+
+def options_add(target: str, field: str, options: Namespace) -> Namespace:
+    options.do = "searchDocument"
+    options.argument = {
+        "field": field,
+        "searchFor": target.split(","),
+        "collection": connect.baseCollection_getAndConnect(options).collectionName,
     }
     return options
 
-def documentSearch_asInt(options:Namespace) ->int:
+
+def documentSearch_asInt(options: Namespace) -> int:
     return driver.run_intReturn(options)
 
-def documentSearch_asModel(options:Namespace) -> responseModel.mongodbResponse:
+
+def documentSearch_asModel(options: Namespace) -> responseModel.mongodbResponse:
     return driver.run_modelReturn(options)
 
-@click.command(cls = env.CustomCommand, help=f"""
-find all {YL}documents{NC} containing strings of interest
+
+@click.command(
+    cls=env.CustomCommand,
+    help=f"""
+find all {PL}documents{NC} containing strings of interest
 
 SYNOPSIS
 {CY}search {GR}--target {YL}<stringList>{NC} [{GR}--field {YL}<field>{NC}]
@@ -43,18 +50,21 @@ If a {YL}document{NC} contains one (or more) of the {YL}<stringList>{NC} targets
 return the value of the {YL}document{NC}'s {YL}<field>{NC}. Typically this is the
 document {YL}_id{NC}.
 
-""")
-@click.option('--target',
-    type        = str,
-    help        = \
-    "A comma separated list. The logical OR of the search is returned",
-    default     = '')
-@click.option('--field',
-    type        = str,
-    help        = \
-    "List the search hits referenced by this field",
-    default     = '_id')
+""",
+)
+@click.option(
+    "--target",
+    type=str,
+    help="A comma separated list. The logical OR of the search is returned",
+    default="",
+)
+@click.option(
+    "--field",
+    type=str,
+    help="List the search hits referenced by this field",
+    default="_id",
+)
 @click.pass_context
-def search(ctx:click.Context, target:str, field:str) -> int:
+def search(ctx: click.Context, target: str, field: str) -> int:
     # pudb.set_trace()
-    return documentSearch_asInt(options_add(target, field, ctx.obj['options']))
+    return documentSearch_asInt(options_add(target, field, ctx.obj["options"]))
