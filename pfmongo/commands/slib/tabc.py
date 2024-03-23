29ede60d197a3b39117eb463ab6fa68744714f72
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-03-22 20:56:46 -0400

    Prompt updates

--- pfmongo/commands/slib/tabc.py
+++ pfmongo/commands/slib/tabc.py
@@ -1,73 +1,37 @@
-import readline
-from typing import Optional
 from pfmongo.commands import smash
+from pfmongo.models import responseModel
+import pfmongo.commands.fop.ls as ls
 from argparse import Namespace
 import pudb
+import ast
 
-testfiles: list[str] = [
-    "lld.json",
-    "aphid",
-    "berry",
-    "balloon",
-    "banana",
-    "ball",
-    "cherry",
-    "cherub",
-    "duck",
-]
-
-choices: list[str] = []
-fscmd: str = ""
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import NestedCompleter
+from prompt_toolkit.formatted_text import ANSI
 
 
-def completer(text: str, state: int) -> Optional[str]:
-    global choices, fscmd
-    hits: list[str] = [choice for choice in choices if choice.startswith(text)]
-    if state < len(hits):
-        for index, item in enumerate(hits):
-            if item in text:
-                choices = testfiles
-                state = index
-                if not fscmd:
-                    fscmd = text
-                break  # Exit the loop after finding the first match
-        return hits[state]
-    else:
-        return None
+def fileList_get(options: Namespace) -> list[str]:
+    lsFiles: list[str] = []
+    lsRet: int = 0
+    lsResp: responseModel.mongodbResponse
+    lsRet, lsResp = ls.ls_do(ls.options_add("", "", False, options))
+    if lsRet:
+        return lsFiles
+    lsFiles = ast.literal_eval(lsResp.message)
+    return lsFiles
 
 
-readline.set_completer(completer)
-readline.parse_and_bind("tab: complete")
+def userInput_get(options: Namespace) -> str:
+    userInput: str = ""
+    files: list[str] = fileList_get(options)
+    sallcmds: set[str] = set(smash.fscommand)
+    snofcmds: set[str] = set(smash.fscommand_noArgs)
+    fcmds: list[str] = list(sallcmds.symmetric_difference(snofcmds))
 
-
-def choice_get(options: Namespace) -> str:
-    global choices, fscmd
-    choices = smash.fscommand
-    fscmd = ""
-    while True:
-        user_input: str = input(smash.prompt_get(options)).strip()
-        # pudb.set_trace()
-        if choices == testfiles:
-            user_input = "".join(user_input.split()[1:])
-        if not user_input:  # If no input, show all choices
-            print("Possible choices:", ", ".join(choices))
-            continue
-        matching_choices: list[str] = [
-            choice for choice in choices if choice.startswith(user_input)
-        ]
-        if len(matching_choices) == 1:  # If only one choice left, return it
-            return matching_choices[0]
-        elif (
-            len(matching_choices) > 1
-        ):  # If multiple choices, prompt again with completion
-            completion: Optional[str] = completer(user_input, 0)
-            if completion is not None:
-                user_input += completion[
-                    len(user_input) :
-                ]  # Update user_input with completion
-                readline.redisplay()
-        elif len([choice for choice in choices if choice in user_input]):
-            return user_input
-        else:
-            print("No matches found. Please try again.")
-            continue
+    d_files: dict = {i: None for i in files}
+    d_choices: dict = {i: d_files for i in fcmds}
+    d_noargs: dict = {i: None for i in snofcmds}
+    d_choices = {**d_choices, **d_noargs}
+    completer = NestedCompleter.from_nested_dict(d_choices)
+    userInput = prompt(ANSI(smash.prompt_get(options)), completer=completer)
+    return userInput
