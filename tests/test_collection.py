ed33a09a38f231222bceaf9b869d9d7272427fb2
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-03-27 10:29:44 -0400

    Refactor

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -1,100 +1,110 @@
-import  builtins
-from    _pytest.capture         import capsys
-from    pfmongo                 import pfmongo
-from    pfmongo.__main__        import main
-from    pfmongo.pfmongo         import options_initialize
-from    argparse                import  ArgumentParser, Namespace
-import  os
-os.environ['XDG_CONFIG_HOME'] = '/tmp'
-import  pudb
-import  pytest
-from    pytest_mock             import mocker
-from    datetime                import datetime
-import  time
-from    typing                  import Any, Callable
-from    unittest.mock           import patch
+from pfmongo import pfmongo
+from pfmongo.__main__ import main
+import os
 
-from    pfmongo.commands.clop   import connect, showAll, deleteCol
-from    pfmongo.models          import responseModel
+from pfmongo.commands.dbop import connect as database
+from pfmongo.commands.clop import connect as collection, showAll as colShow, deleteCol
+from pfmongo.models import responseModel
+
+os.environ["XDG_CONFIG_HOME"] = "/tmp"
 
 
-def test_database_connect_moduleAsInt(capsys) -> None:
-    """ connect to a database "testDB" using a module call with an int return """
+def DB_connect(DB: str = "testDB") -> int:
+    return database.connectTo_asInt(
+        database.options_add(DB, pfmongo.options_initialize())
+    )
 
-    ret:int = connect.connectTo_asInt(connect.options_add("testDB", pfmongo.options_initialize()))
+
+def test_collection_connect_moduleAsInt() -> None:
+    """connect to a database "testDB" using a module call with an int return"""
+    DB_connect()
+    ret: int = collection.connectTo_asInt(
+        collection.options_add("testCollection", pfmongo.options_initialize())
+    )
     assert ret == 0
 
+
 def test_collection_connect_main(capsys) -> None:
-    """ connect to a collection via click """
-    ret:int     = 0
+    """connect to a collection via click"""
+    ret: int = 0
+    DB_connect()
     try:
-        ret     = main(['collection', 'connect', 'testCollection'])
+        ret = main("collection connect testCollection".split())
     except SystemExit:
         pass
-    captured    = capsys.readouterr()
+    captured = capsys.readouterr()
     assert ret == 0
     assert 'Successfully connected collection to "testCollection"' in captured.out
 
-def test_collection_connect_moduleAsInt(capsys) -> None:
-    """ connect to a collection "testCollection" using a module call with an int return """
 
-    ret:int = connect.connectTo_asInt(connect.options_add("testCollection", pfmongo.options_initialize()))
-    assert ret == 0
+def test_collection_connect_moduleAsModel() -> None:
+    """connect to a collection "testCollection" using a module call with model return"""
 
-def test_database_connect_moduleAsModel(capsys) -> None:
-    """ connect to a collection "testCollection" using a module call with model return """
+    DB_connect()
+    ret: responseModel.mongodbResponse = collection.connectTo_asModel(
+        collection.options_add("testCollection", pfmongo.options_initialize())
+    )
+    assert "Successfully" in ret.message
 
-    ret:responseModel.mongodbResponse = \
-        connect.connectTo_asModel(connect.options_add("testCollection", pfmongo.options_initialize()))
-    assert 'Successfully' in ret.message
 
 def test_collection_showall_main(capsys) -> None:
-    """ show all collections via click """
-    ret:int     = 0
+    """show all collections via click"""
+    ret: int = 0
+    DB_connect("admin")
     try:
-        ret     = main(['collection', 'showall'])
+        ret = main("--useDB admin collection showall".split())
     except SystemExit:
         pass
-    captured    = capsys.readouterr()
+    captured = capsys.readouterr()
     assert ret == 0
-    assert 'admin' in captured.out
+    assert "system" in captured.out
 
-def test_collection_showall_moduleAsInt(capsys) -> None:
-    """ show all databases using a module call with an int return """
 
-    ret:int = showAll.showAll_asInt(showAll.options_add(pfmongo.options_initialize()))
+def test_collection_showall_moduleAsInt() -> None:
+    """show all collections using a module call with an int return"""
+
+    DB_connect("admin")
+    ret: int = colShow.showAll_asInt(colShow.options_add(pfmongo.options_initialize()))
     assert ret == 0
 
-def test_collection_showAll_moduleAsModel(capsys) -> None:
-    """ show all databases using a module call with model return """
 
-    ret:responseModel.mongodbResponse = \
-        showAll.showAll_asModel(showAll.options_add(pfmongo.options_initialize()))
-    assert 'admin' in ret.message
+def test_collection_showAll_moduleAsModel() -> None:
+    """show all collections using a module call with model return"""
+    DB_connect("admin")
+    ret: responseModel.mongodbResponse = colShow.showAll_asModel(
+        colShow.options_add(pfmongo.options_initialize())
+    )
+    assert "system" in ret.message
+
 
 def test_collection_delete_main(capsys) -> None:
-    """ delete collection via click """
-    ret:int     = 0
+    """delete collection via click"""
+    ret: int = 0
     try:
-        ret     = main(['--useCollection', 'testCollection', 'collection', 'deletecol', 'testCollection'])
+        ret = main(
+            "--useCollection testCollection collection deletecol testCollection".split()
+        )
     except SystemExit:
         pass
-    captured    = capsys.readouterr()
+    captured = capsys.readouterr()
     assert ret == 0
-    assert 'Successfully' in captured.out
+    assert "Successfully" in captured.out
+
 
 def test_collection_delete_moduleAsInt(capsys) -> None:
-    """ delete databases using a module call with an int return """
+    """delete databases using a module call with an int return"""
 
-    ret:int = deleteCol.collectiondel_asInt(
-                deleteCol.options_add("testCollection", pfmongo.options_initialize()))
+    ret: int = deleteCol.collectiondel_asInt(
+        deleteCol.options_add("testCollection", pfmongo.options_initialize())
+    )
     assert ret == 0
 
+
 def test_collection_delete_moduleAsModel(capsys) -> None:
-    """ show all databases using a module call with model return """
+    """show all databases using a module call with model return"""
 
-    ret:responseModel.mongodbResponse = \
-        deleteCol.collectiondel_asModel(
-                deleteCol.options_add("testCollection", pfmongo.options_initialize()))
+    ret: responseModel.mongodbResponse = deleteCol.collectiondel_asModel(
+        deleteCol.options_add("testCollection", pfmongo.options_initialize())
+    )
 
-    assert 'Successfully' in ret.message
+    assert "Successfully" in ret.message
