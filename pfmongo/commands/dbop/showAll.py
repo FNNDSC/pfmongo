33faac3ff12376a2c57605ba8a3083225a3a81c9
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-06-07 09:48:32 -0400

    All driver methods become async

--- pfmongo/commands/dbop/showAll.py
+++ pfmongo/commands/dbop/showAll.py
@@ -1,27 +1,43 @@
-import  click
-from    argparse        import  Namespace
-from    pfmisc          import  Colors as C
-from    pfmongo         import  driver, env
-from    pfmongo.models  import  responseModel
-import  copy
+import asyncio
+import click
+from argparse import Namespace
+from pfmisc import Colors as C
+from pfmongo import driver, env
+from pfmongo.models import responseModel
+import copy
+import pudb
 
 NC = C.NO_COLOUR
 GR = C.LIGHT_GREEN
 CY = C.CYAN
 PL = C.PURPLE
 
-def options_add(options:Namespace) -> Namespace:
-    localoptions:Namespace = copy.deepcopy(options)
-    localoptions.do        = 'showAllDB'
+
+def options_add(options: Namespace) -> Namespace:
+    localoptions: Namespace = copy.deepcopy(options)
+    localoptions.do = "showAllDB"
     return localoptions
 
-def showAll_asInt(options:Namespace) -> int:
-    return driver.run_intReturn(options)
 
-def showAll_asModel(options:Namespace) -> responseModel.mongodbResponse:
-    return driver.run_modelReturn(options)
+async def showAll_asInt(options: Namespace) -> int:
+    return await driver.run_intReturn(options)
 
-@click.command(cls = env.CustomCommand, help=f"""
+
+async def showAll_asModel(options: Namespace) -> responseModel.mongodbResponse:
+    return await driver.run_modelReturn(options)
+
+
+def sync_showAll_asInt(options: Namespace) -> int:
+    return asyncio.run(showAll_asInt(options))
+
+
+def sync_showAll_asModel(options: Namespace) -> responseModel.mongodbResponse:
+    return asyncio.run(showAll_asModel(options))
+
+
+@click.command(
+    cls=env.CustomCommand,
+    help=f"""
 list {PL}DATABASES{NC} containing data
 
 SYNOPSIS
@@ -31,8 +47,9 @@ DESC
 This command shows all the populated databases available in a given mongodb
 server. It accepts no arguments.
 
-""")
+""",
+)
 @click.pass_context
-def showAll(ctx:click.Context) -> int:
-    # pudb.set_trace()
-    return showAll_asInt(options_add(ctx.obj['options']))
+def showAll(ctx: click.Context) -> int:
+    # return showAll_asInt(options_add(ctx.obj["options"]))
+    return sync_showAll_asInt(options_add(ctx.obj["options"]))
