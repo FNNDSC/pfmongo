be7100b4e115282239cf3249734940301bc99205
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-06-10 10:55:38 -0400

    Fix async integration and add history

--- pfmongo/commands/slib/tabc.py
+++ pfmongo/commands/slib/tabc.py
@@ -5,18 +5,22 @@ from argparse import Namespace
 import pudb
 import ast
 
-from prompt_toolkit import prompt
+from prompt_toolkit import prompt, PromptSession
 from prompt_toolkit.completion import NestedCompleter
 from prompt_toolkit.formatted_text import ANSI
-from prompt_toolkit.layout import Layout, HSplit, VSplit, FloatContainer, Float
+
+# from prompt_toolkit.layout import Layout, HSplit, VSplit, FloatContainer, Float
+from prompt_toolkit.history import InMemoryHistory
 import re
 
+history: InMemoryHistory = InMemoryHistory()
 
-def fileList_get(options: Namespace) -> list[str]:
+
+async def fileList_get(options: Namespace) -> list[str]:
     lsFiles: list[str] = []
     lsRet: int = 0
     lsResp: responseModel.mongodbResponse
-    lsRet, lsResp = ls.ls_do(ls.options_add("", "", False, options))
+    lsRet, lsResp = await ls.ls_do(ls.options_add("", "", False, options))
 
     if lsRet:
         return lsFiles
@@ -28,7 +32,7 @@ def fileList_get(options: Namespace) -> list[str]:
     return lsFiles
 
 
-def userInput_get(options: Namespace, **kwargs) -> str:
+async def userInput_get(options: Namespace, **kwargs) -> str:
     noninteractive: str = ""
     for k, v in kwargs.items():
         if k == "noninteractive":
@@ -36,15 +40,16 @@ def userInput_get(options: Namespace, **kwargs) -> str:
     if noninteractive:
         return noninteractive
     userInput: str = ""
-    files: list[str] = fileList_get(options)
+    files: list[str] = await fileList_get(options)
     sallcmds: set[str] = set(smash.fscommand)
     snofcmds: set[str] = set(smash.fscommand_noArgs)
     fcmds: list[str] = list(sallcmds.symmetric_difference(snofcmds))
-
     d_files: dict = {i: None for i in files}
     d_choices: dict = {i: d_files for i in fcmds}
     d_noargs: dict = {i: None for i in snofcmds}
     d_choices = {**d_choices, **d_noargs}
     completer = NestedCompleter.from_nested_dict(d_choices)
-    userInput = prompt(ANSI(smash.prompt_get(options)), completer=completer)
+    session: PromptSession = PromptSession(history=history)
+    smashprompt: str = await smash.prompt_get(options)
+    userInput = await session.prompt_async(ANSI(smashprompt), completer=completer)
     return userInput
