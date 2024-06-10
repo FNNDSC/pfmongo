9a0efc5d49ce4014699e96cfcb6cbaf6e3325c43
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-06-10 10:58:16 -0400

    Add pervasive async integration

--- pfmongo/commands/fop/prompt.py
+++ pfmongo/commands/fop/prompt.py
@@ -1,60 +1,82 @@
-import  click
-from    pfmongo         import  driver, env
-from    argparse        import  Namespace
-from    pfmisc          import  Colors as C
-from    pfmongo.models  import  responseModel, fsModel
-from    pathlib         import  Path
-import  copy
-import  ast
-import  pudb
+import click
+from pfmongo import driver, env
+from argparse import Namespace
+from pfmisc import Colors as C
+from pfmongo.models import responseModel, fsModel
+from pathlib import Path
+import copy
+import ast
+import pudb
+import asyncio
 
-from    pfmongo.commands.document   import showAll as doc
-from    pfmongo.commands.state      import showAll as state
-import  pfmongo.commands.smash      as smash
-import  pfmongo.commands.fop.cd     as cd
+from pfmongo.commands.document import showAll as doc
+from pfmongo.commands.state import showAll as state
+import pfmongo.commands.smash as smash
+import pfmongo.commands.fop.cd as cd
 
 
-NC  = C.NO_COLOUR
-GR  = C.GREEN
-CY  = C.CYAN
-PL  = C.PURPLE
-YL  = C.YELLOW
+NC = C.NO_COLOUR
+GR = C.GREEN
+CY = C.CYAN
+PL = C.PURPLE
+YL = C.YELLOW
 
-def options_add(options:Namespace) -> Namespace:
-    localoptions:Namespace              = copy.deepcopy(options)
-    localoptions.beQuiet                = True
-    cwd:responseModel.mongodbResponse   = state.showAll_asModel(localoptions)
-    localoptions.cwd                    = '/' +  cwd.message
+
+async def options_add(options: Namespace) -> Namespace:
+    localoptions: Namespace = copy.deepcopy(options)
+    localoptions.beQuiet = True
+    cwd: responseModel.mongodbResponse = await state.showAll_asModel(localoptions)
+    localoptions.cwd = "/" + cwd.message
     return localoptions
 
-def cd_options(options:Namespace) -> Namespace:
-    mkdir:bool  = True
+
+def cd_options(options: Namespace) -> Namespace:
+    mkdir: bool = True
     return cd.options_add(options.cwd, options, mkdir)
 
-def prompt_do(options:Namespace) -> fsModel.cdResponse:
-    promptResp:fsModel.cdResponse       = cd.changeDirectory(cd_options(options))
-    (db, collection)                    = cd.path_to_dbCol(cd_options(options))
+
+async def prompt_do(options: Namespace) -> fsModel.cdResponse:
+    # pudb.set_trace()
+    promptResp: fsModel.cdResponse = await cd.changeDirectory(cd_options(options))
+    (db, collection) = cd.path_to_dbCol(cd_options(options))
     if db:
-        promptResp.message  = '/'
-        match promptResp.state['database']:
-            case 'void':    promptResp.message  += f"{YL}{db}{NC}"
-            case 'exists':  promptResp.message  += f"{GR}{db}{NC}"
+        promptResp.message = "/"
+        match promptResp.state["database"]:
+            case "void":
+                promptResp.message += f"{YL}{db}{NC}"
+            case "exists":
+                promptResp.message += f"{GR}{db}{NC}"
     if collection:
-        promptResp.message += '/'
-        match promptResp.state['collection']:
-            case 'void':    promptResp.message  += f"{YL}{collection}{NC}"
-            case 'exists':  promptResp.message  += f"{GR}{collection}{NC}"
+        promptResp.message += "/"
+        match promptResp.state["collection"]:
+            case "void":
+                promptResp.message += f"{YL}{collection}{NC}"
+            case "exists":
+                promptResp.message += f"{GR}{collection}{NC}"
     return promptResp
 
-def prompt_asModel(options) -> fsModel.cdResponse:
-    return prompt_do(options)
 
-def prompt_asInt(options) -> int:
-    pathResp:fsModel.cdResponse = prompt_do(options)
+async def prompt_asModel(options: Namespace) -> fsModel.cdResponse:
+    return await prompt_do(await options_add(options))
+
+
+async def prompt_asInt(options: Namespace) -> int:
+    pathResp: fsModel.cdResponse = await prompt_do(await options_add(options))
     print(pathResp.message)
     return pathResp.code
 
-@click.command(cls = env.CustomCommand, help=f"""
+
+def sync_prompt_asInt(options: Namespace) -> int:
+    return asyncio.run(prompt_asInt(options))
+
+
+def sync_prompt_asModel(options: Namespace) -> fsModel.cdResponse:
+    return asyncio.run(prompt_asModel(options))
+
+
+@click.command(
+    cls=env.CustomCommand,
+    help=f"""
 return a {YL}color {CY}aware{NC} prompt
 
 SYNOPSIS
@@ -70,8 +92,9 @@ in terms of file system analogues is that in a file system a {YL}mkdir{NC}
 command actually creates something that at first is empty. In monogo, a
 {YL}database{NC} or {YL}collection{NC} is only "really real" once it is not empty.
 
-""")
+""",
+)
 @click.pass_context
-def prompt(ctx:click.Context) -> int:
+def prompt(ctx: click.Context) -> int:
     # pudb.set_trace()
-    return prompt_asInt(options_add(ctx.obj['options']))
+    return sync_prompt_asInt(ctx.obj["options"])
